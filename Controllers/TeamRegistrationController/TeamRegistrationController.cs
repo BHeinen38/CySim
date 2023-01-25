49dabe40d7da91a78d7ee1dc9e031badd86f4902
bailey <bjheinen@iastate.edu>
2023-01-25 08:03:28 -0600

    adding join option to the team registration table

--- Controllers/TeamRegistrationController/TeamRegistrationController.cs
+++ Controllers/TeamRegistrationController/TeamRegistrationController.cs
@@ -92,8 +92,15 @@ namespace CySim.Controllers.TeamRegistrationController
             }
             return View(registration);
         }
-    
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+        
+        [HttpGet]
+        public IActionResult Join()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
