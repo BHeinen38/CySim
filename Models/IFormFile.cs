8ce2c4bc049b23c68c91f665670fc86e58a7788e
bailey <bjheinen@iastate.edu>
2023-02-11 12:08:45 -0600

    start to maybe a better way to deal with tutorials

---
+++ Models/IFormFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CySim.Models
+{
+    public interface IFormFile
+    {
+        string ContentType { get; }
+        string ContentDisposition { get; }
+        IHeaderDictionary Headers { get; }
+        long Length { get; }
+        string Name { get; }
+        string FileName { get; }
+        Stream OpenReadStream();
+        void CopyTo(Stream target);
+        Task CopyToAsync(Stream target, CancellationToken? cancellationToken = null);
+    }
+}
+
