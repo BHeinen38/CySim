026e52388b63d525c4a6b25180e1abb3f3af87a1
Matthew <35633976+MattDaoud225@users.noreply.github.com>
2023-03-09 13:17:45 -0600

    Added force CSS recache for making site easier to design. Working on final design tweaks for scoreboard before backend implementation.

---
+++ Migrations/20230309174126_Matthew2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CySim.Migrations
+{
+    public partial class Matthew2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Flags",
+                table: "ScoreBoards",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Flags",
+                table: "ScoreBoards");
+        }
+    }
+}
