f3c5aae2660251d36ab78b9e70a4879eec2e9b5b
bailey <bjheinen@iastate.edu>
2023-03-31 18:23:50 -0500

    Adding a scoring implementation

--- Controllers/ScoreBoardController/ScoreBoardController.cs
+++ Controllers/ScoreBoardController/ScoreBoardController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using CySim.Models.ScoreBoardModels;
+using CySim.Models.TeamRegistration;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,6 +30,35 @@ namespace CySim.Controllers.ScoreBoardController
         public IActionResult ScoreBoard()
         {
             return View(_context.TeamRegistrations.ToList().OrderByDescending(x => x.Score));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var registration = _context.TeamRegistrations.Find(id);
+
+            if (registration == null)
+                return NotFound();
+
+
+            return View(registration);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TeamRegistration registration)
+        {
+            var teamRegistration = _context.TeamRegistrations.Find(registration.Id);
+
+            teamRegistration.Score = registration.Score;
+            teamRegistration.Usability = registration.Usability;
+            teamRegistration.Flags = registration.Flags;
+
+            _context.Update(teamRegistration);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(ScoreBoard));
+            
+           
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
