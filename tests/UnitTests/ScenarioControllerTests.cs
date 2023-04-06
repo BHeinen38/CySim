044edd56831613fb526245a47b56aab63b3bfbdc
p0l1t1c1an <boicken19@gmail.com>
2023-04-06 03:57:02 -0500

    Fixed unit test to only a single database

--- tests/UnitTests/ScenarioControllerTests.cs
+++ tests/UnitTests/ScenarioControllerTests.cs
@@ -8,26 +8,24 @@ using Microsoft.EntityFrameworkCore.InMemory;
 using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
 
-public class ScenarioControllerTests
+public class ScenarioFixture
 {
-    private (ApplicationDbContext, ScenarioController, List<Scenario>) InitializeTest()
+    public ApplicationDbContext context  { get; private set; }
+    public List<Scenario> scenarios      { get; private set; }
+
+    public ScenarioFixture()
     {
-        // Set up logger and mock db to create controller  
-        var logger = NullLogger<ScenarioController>.Instance;
-        
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
         optionsBuilder.UseInMemoryDatabase("CySim");
         
-        var context = new ApplicationDbContext(optionsBuilder.Options);
+        context = new ApplicationDbContext(optionsBuilder.Options);
 
         // Creates database tables and columns if they don't exist
         context.Database.EnsureDeleted();
         context.Database.EnsureCreated();
         
-        var controller = new ScenarioController(logger, context); 
-        
         // Initialize scenarios in db
-        var scenarios = new List<Scenario>{
+        scenarios = new List<Scenario>{
             new Scenario{Id = 1, FileName  = "FirstFile", FilePath = "Test/FirstFile", Description = "First Test Scenario", isRed = false}, 
             new Scenario{Id = 2, FileName  = "SecondFile", FilePath = "Test/SecondFile", Description = "Second Test Scenario", isRed = true}, 
             new Scenario{Id = 3, FileName  = "ThirdFile", FilePath = "Test/ThirdFile", Description = "Third Test Scenario", isRed = false} 
@@ -35,28 +33,39 @@ public class ScenarioControllerTests
         
         context.Scenarios.AddRange(scenarios);
         context.SaveChanges(); 
-
-        return (context, controller, scenarios);
     }
+}
 
-       
+
+public class ScenarioGetTests : IClassFixture<ScenarioFixture>
+{
+    private readonly ScenarioFixture _fixture;
+    
+    public ScenarioGetTests(ScenarioFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
     [Fact]
     public async Task IndexGet() 
     { 
-        (_, var controller, _) = InitializeTest(); 
-
+        var logger = NullLogger<ScenarioController>.Instance;
+        var controller = new ScenarioController(logger, _fixture.context); 
+ 
         // Query Index 
         var result = await controller.Index();
         
         // Confirm that a view is returned and has correct model type
         var viewResult = Assert.IsType<ViewResult>(result);
         var model = Assert.IsAssignableFrom<List<Scenario>>(viewResult.ViewData.Model);
+        Assert.Equal(_fixture.scenarios.OrderBy(x => x.isRed), model); 
     }
 
     [Fact]
     public void CreateGet() 
     { 
-        (_, var controller, _) = InitializeTest(); 
+        var logger = NullLogger<ScenarioController>.Instance;
+        var controller = new ScenarioController(logger, _fixture.context); 
 
         // Query Index 
         var result = controller.Create();
@@ -72,7 +81,8 @@ public class ScenarioControllerTests
     [InlineData(3)]
     public async Task EditGet(int id) 
     { 
-        (_, var controller, var scenarios) = InitializeTest(); 
+        var logger = NullLogger<ScenarioController>.Instance;
+        var controller = new ScenarioController(logger, _fixture.context); 
 
         // Query Index 
         var result = await controller.Edit(id);
@@ -83,6 +93,6 @@ public class ScenarioControllerTests
    
         /// Confirm Model is the one queried by id number
         Assert.NotNull(model);    
-        Assert.Equal(scenarios[id-1], model);
+        Assert.Equal(_fixture.scenarios[id-1], model);
     }
 }
