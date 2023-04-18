7518562765f0f4ba75b7a92e3ebe5becc205a0e4
bailey <bjheinen@iastate.edu>
2023-04-18 13:16:16 -0500

    adding a migrations

---
+++ Migrations/20230418181408_updatedMerge_tutorial_scenario.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CySim.Migrations
+{
+    public partial class updatedMerge_tutorial_scenario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
